5e3413198a925acce13ab7b527d8fd003edc7f51
Consultant <thompsondemetricjr@gmail.com>
2023-09-27 10:22:46 -0400

    updated

--- bd_us/Postgres_To_Hive.py
+++ bd_us/Postgres_To_Hive.py
@@ -16,23 +16,11 @@ df = spark.read \
 
 
 df.show()
-#
-# # spark.sql("CREATE DATABASE fraud_db")
-# spark.sql("SHOW DATABASES").show()
-# spark.sql("USE fraud_db").show()
-# # Assuming you already have an SQLContext
-# # # Provide your SQL query as a string
-# view_name = "fraud_view"
+
 database_name = "fraud_db"
 table_name = "fraud_table"
-# #
-#
-# df.createOrReplaceTempView(view_name)
+
 df.write.mode("overwrite").saveAsTable(database_name+"."+table_name)
-# # spark.sql("drop table if exists "+table_name)
-# spark.sql("CREATE TABLE if NOT EXISTS " + table_name+ " AS SELECT * FROM " + view_name)
-# df.write.saveAsTable("fraud_spark",mode= "overwrite")
-# df.write.save("fraud_spark", format="hive")
 
 # Stop the SparkSession
 # spark.stop()
